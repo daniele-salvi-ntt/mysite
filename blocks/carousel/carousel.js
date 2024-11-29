1623797c19d30001f9e11e2752c0c8ccb176c430
Patrick Pulcini <patrick.pulcini@nttdata.com>
2024-11-29 16:28:15 +0100

    added animation and adjusted sizing

--- blocks/carousel/carousel.js
+++ blocks/carousel/carousel.js
@@ -1,8 +1,33 @@
-function changeElement(imageList){
+var timer = 5;
+async function changeElement(imageList){
   const nextCandidate = $('.carousel-item[active] ~ .carousel-item',imageList)[0];
   const nextImage = nextCandidate != null ? nextCandidate : $('.carousel-item',imageList)[0]
-  $('.carousel-item[active]',imageList).attr('active',null); 
+  const out = $('.carousel-item[active]',imageList)
+  animateInOut(out,nextImage);
+  timer=5;
+  
+}
+
+function animateInOut(out,nextImage) {
+  let id = null;
+  let pos = 0;
+  clearInterval(id);
   $(nextImage).attr('active','');
+  $(nextImage).css('z-index',1);
+  $(out).css('z-index',2);
+  id = setInterval(frame, 10);
+    function frame() {
+    if (pos == 100) {
+      clearInterval(id);
+      $(out).attr('active',null);
+      $(out).css('transform','translateX(0%)')
+    } else {
+      pos++;
+      $(out).css('transform', 'translateX('+pos+'%)');
+      $(nextImage).css('transform', 'translateX('+50-pos+'%)');
+    }
+  }
+
 }
 
 export default async function decorate(block) {
@@ -15,12 +40,17 @@ export default async function decorate(block) {
     if(i == 0){
       $(image).attr('active','');
     }
-    $(image).append(images[i].firstElementChild);
+    $(image).append(images[i].firstElementChild.firstElementChild);
     $(imageList).append($(image));
   }
   $(carousel).append(imageList);
   $(carousel).on('click',()=>{changeElement($(imageList))})
-  window.setInterval(()=>{changeElement($(imageList))}, 5000);
+  window.setInterval(()=>{
+    timer-=1;
+    if(timer==0){
+      changeElement($(imageList))
+    }
+  }, 1000);
   $(block).append(carousel);
 
 }
